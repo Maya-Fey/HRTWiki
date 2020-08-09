d93e7c59291733e8eaabdc2b8acd3e362343c5f2
Claire <foxcoindev@gmail.com>
2020-08-09 12:08:14 +1200

    Added null helper class

---
+++ src/main/java/claire/hrt/wiki/commons/Null.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package claire.hrt.wiki.commons;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * @author Claire
+ *
+ * Simple helper class to deal with library methods that don't use annotations
+ */
+public class Null {
+	
+	@NonNull
+	public static <T> T nonNull(@Nullable T t)
+	{
+		if(t == null) 
+			throw new PreconditionViolationException("Incorrect assumption, t was actually null.");
+		return t;
+	}
+
+}
