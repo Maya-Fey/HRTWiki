d0d5c363ddfcce4c5af59bb69012060998bc02ed
Claire <foxcoindev@gmail.com>
2020-08-09 13:08:05 +1200

    Documented contract exceptions

--- src/main/java/claire/hrt/wiki/commons/except/PreconditionViolationException.java
+++ src/main/java/claire/hrt/wiki/commons/except/PreconditionViolationException.java
@@ -1,23 +1,47 @@
 package claire.hrt.wiki.commons.except;
 
+/**
+ * For when the method arguments do not meet the contract of a method.
+ * 
+ * @author Claire
+ */
 public class PreconditionViolationException extends Error {
 
 	private static final long serialVersionUID = 764236203625490841L;
 
+	/**
+	 * 
+	 */
 	public PreconditionViolationException() {}
 
+	/**
+	 * @param message
+	 */
 	public PreconditionViolationException(String message) {
 		super(message);
 	}
 
+	/**
+	 * @param cause
+	 */
 	public PreconditionViolationException(Throwable cause) {
 		super(cause);
 	}
 
+	/**
+	 * @param message
+	 * @param cause
+	 */
 	public PreconditionViolationException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
 	public PreconditionViolationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}
