5f74cecfd33fdfe85e3aa1921f0db10eb80f8d85
Claire <foxcoindev@gmail.com>
2020-08-09 13:43:58 +1200

    Implemented Data Context

---
+++ src/main/java/claire/hrt/wiki/data/DataContextImpl.java
@@ -0,0 +1,67 @@
+package claire.hrt.wiki.data;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import claire.hrt.wiki.commons.except.PreconditionViolationException;
+import claire.hrt.wiki.data.virtual.DataContext;
+
+/**
+ * An implementation of DataContext
+ * 
+ * @author Claire
+ */
+public class DataContextImpl implements DataContext {
+
+	private static final String DEFAULT_PATH = "application_data/";
+	
+	private final String rootPath;
+	
+	/**
+	 * Creates a data context from a given file path
+	 * 
+	 * @param rootPath The root path to use for all files provided by this context
+	 */
+	public DataContextImpl(String rootPath)
+	{
+		this.rootPath = rootPath;
+		File file = new File(rootPath);
+		if(file.exists()) {
+			if(file.isFile()) {
+				throw new PreconditionViolationException("Given root path points to a file");
+			}
+		} else {
+			file.mkdir();
+		}
+	}
+	
+	/**
+	 * Creates a data contacts from the deafault root file path
+	 */
+	public DataContextImpl()
+	{
+		this(DEFAULT_PATH);
+	}
+
+	@Override
+	public FileInputStream getReadStream(String domain, String name) throws IOException {
+		File file = new File(this.rootPath + "/" + domain + "/" + name);
+		if(!file.exists()) file.createNewFile();
+		return new FileInputStream(file);
+	}
+
+	@Override
+	public FileOutputStream getWriteStream(String domain, String name) throws IOException {
+		File file = new File(this.rootPath + "/" + domain + "/" + name);
+		if(!file.exists()) file.createNewFile();
+		return new FileOutputStream(file);
+	}
+
+	@Override
+	public void dispose() {
+		//Nothing here
+	}
+
+}
--- src/main/java/claire/hrt/wiki/data/virtual/DataContext.java
+++ src/main/java/claire/hrt/wiki/data/virtual/DataContext.java
@@ -1,5 +1,9 @@
 package claire.hrt.wiki.data.virtual;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Programming context for database read/write
  * 
@@ -7,6 +11,26 @@ package claire.hrt.wiki.data.virtual;
  */
 public interface DataContext {
 	
+	/**
+	 * @param domain The domain for this file
+	 * @param name The name of this file
+	 * @return A stream capable of reading to the file
+	 * @throws IOException If there is an error in creating, finding, or reading from the file
+	 */
+	FileInputStream getReadStream(String domain, String name) throws IOException;
+	
+	/**
+	 * @param domain The domain for this file
+	 * @param name The name of this file
+	 * @return A stream capable of writing to the file
+	 * @throws IOException If there is an error in creating, finding, or writing to the file
+	 */
+	FileOutputStream getWriteStream(String domain, String name) throws IOException;
+	
+	/**
+	 * Disposes of this context, destroying any file associations and closing
+	 * any network connections
+	 */
 	void dispose();
 
 }
