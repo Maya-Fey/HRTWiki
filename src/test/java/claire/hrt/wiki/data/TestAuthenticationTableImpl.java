a95a2c549cb96d90ad4f38eb30325ea0f2bb2232
Claire <foxcoindev@gmail.com>
2020-08-11 12:56:31 +1200

    Added initialize/flush tests to TestAuthenticationTableImpl

--- src/test/java/claire/hrt/wiki/data/TestAuthenticationTableImpl.java
+++ src/test/java/claire/hrt/wiki/data/TestAuthenticationTableImpl.java
@@ -5,7 +5,12 @@ package claire.hrt.wiki.data;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +18,7 @@ import claire.hrt.wiki.commons.Null;
 import claire.hrt.wiki.data.except.DuplicateKeyException;
 import claire.hrt.wiki.data.except.NoSuchKeyException;
 import claire.hrt.wiki.data.virtual.AuthenticationTable;
+import claire.hrt.wiki.data.virtual.DataContext;
 
 /**
  * Tests authentication table
@@ -95,5 +101,35 @@ public class TestAuthenticationTableImpl {
 		assertEquals(LoginReturn.LOGIN_SUCCESS, auth.read("Claire", Null.nonNull("anotherpassword".toCharArray())));
 	}
 	
+	/**
+	 * Tests the save/load functionality
+	 */
+	@SuppressWarnings("static-method")
+	@Test
+	public void testTestSaveLoad()
+	{
+		try {
+			Path file = Files.createTempDirectory("mytemp-");
+			String s = Null.nonNull(file.toFile().getCanonicalPath());
+			DataContext context = new DataContextImpl(s);
+			{
+				AuthenticationTable auth = new AuthenticationTableImpl();
+				try {
+					auth.write("Claire", Null.nonNull("clairespassword".toCharArray()));
+					auth.write("Claire2", Null.nonNull("anotherpassword".toCharArray()));
+				} catch (DuplicateKeyException e) { fail(e); }
+				auth.flush(context);
+			}
+			{
+				AuthenticationTable auth = new AuthenticationTableImpl();
+				auth.initialize(context);
+				assertTrue(auth.isInitialized());
+				assertEquals(LoginReturn.LOGIN_SUCCESS, auth.read("Claire", Null.nonNull("clairespassword".toCharArray())));
+				assertEquals(LoginReturn.LOGIN_SUCCESS, auth.read("Claire2", Null.nonNull("anotherpassword".toCharArray())));
+			}
+		} catch(IOException e) {
+			fail(e);
+		}
+	}
 	
 }
