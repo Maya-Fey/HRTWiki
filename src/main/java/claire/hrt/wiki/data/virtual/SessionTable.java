c76b27792e60c3749b6ef611153f4181da5e164b
Claire <foxcoindev@gmail.com>
2020-08-14 12:19:06 +1200

    Added session table interface

---
+++ src/main/java/claire/hrt/wiki/data/virtual/SessionTable.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package claire.hrt.wiki.data.virtual;
+
+import claire.hrt.wiki.data.Session;
+import claire.hrt.wiki.data.except.NoSuchKeyException;
+
+/**
+ * An interface for the session table.
+ * 
+ * The session table contains all active sessions, as well as records sessions.
+ * 
+ * @author Claire
+ */
+public interface SessionTable extends DataTable {
+	
+	/**
+	 * @return A brand new session with a unique ID
+	 */
+	Session newSession();
+	
+	/**
+	 * @param id The session ID
+	 * @return Whether or not the ID represents a valid session
+	 */
+	boolean isActiveSession(String id);
+	
+	/**
+	 * @param id The session ID
+	 * @return The session represented by the ID
+	 * @throws NoSuchKeyException is the session ID does not represent a valid session
+	 */
+	Session getSessionByID(String id) throws NoSuchKeyException;
+
+}
