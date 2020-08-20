ce7a75b0c8ca0f4bd1b534c1182d58ec948917c8
Claire <foxcoindev@gmail.com>
2020-08-20 17:10:52 +1200

    Added validator to LoginServlet

--- src/main/java/claire/hrt/wiki/server/LoginServlet.java
+++ src/main/java/claire/hrt/wiki/server/LoginServlet.java
@@ -14,6 +14,10 @@ import claire.hrt.wiki.data.Session;
 import claire.hrt.wiki.data.WebState;
 import claire.hrt.wiki.data.enumerate.LoginReturn;
 import claire.hrt.wiki.data.except.NoSuchKeyException;
+import claire.hrt.wiki.server.validate.CharacterValidator;
+import claire.hrt.wiki.server.validate.Field;
+import claire.hrt.wiki.server.validate.LengthValidator;
+import claire.hrt.wiki.server.validate.RequestValidator;
 
 /**
  * @author Claire
@@ -21,6 +25,14 @@ import claire.hrt.wiki.data.except.NoSuchKeyException;
 public class LoginServlet extends AbstractStateServlet {
 
 	private static final long serialVersionUID = -6986577362318872849L;
+	
+	private static final String USERNAME_ALLOWED = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-";
+	private static final String PASSWORD_ALLOWED = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()-=_+";
+	
+	private static final RequestValidator validator = new RequestValidator(
+				new Field("username", true, new LengthValidator(1, 32), new CharacterValidator(USERNAME_ALLOWED)),
+				new Field("password", true, new LengthValidator(8, 64), new CharacterValidator(PASSWORD_ALLOWED)) 
+			);
 	
 	@Override
     protected void get(HttpServletRequest request, Session session, HttpServletResponse response) throws ServletException, IOException {
@@ -37,11 +49,12 @@ public class LoginServlet extends AbstractStateServlet {
 			response.sendRedirect("/admin/index.jsp");
 			return;
 		}
-		//TODO: IP rate limiting
-		//TODO: Add additional checks
-		if(request.getParameter("username") == null || request.getParameter("password") == null) {
-			request.setAttribute("failure_reason", "Invalid Request");
+		
+		String err = validator.validate(request);
+		if(err.length() > 0) {
+			request.setAttribute("failure_reason", err);
 			this.get(request, session, response);
+			return;
 		}
 		
         LoginReturn result = WebState.INST.auth.read(Null.nonNull(request.getParameter("username")), Null.nonNull(request.getParameter("password").toCharArray()));
