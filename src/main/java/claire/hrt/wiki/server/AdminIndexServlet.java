8ecff6c5a90b3f4314d1abdcbaeb2d46a0f6f467
Claire <foxcoindev@gmail.com>
2020-08-21 13:46:24 +1200

    Added Admin/Index servlet

---
+++ src/main/java/claire/hrt/wiki/server/AdminIndexServlet.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package claire.hrt.wiki.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import claire.hrt.wiki.data.Session;
+import claire.hrt.wiki.data.enumerate.UserRole;
+
+/**
+ * @author Claire
+ */
+public class AdminIndexServlet extends AbstractStateServlet {
+
+	private static final long serialVersionUID = -6986577362318872849L;
+	
+	@Override
+    protected void get(HttpServletRequest request, Session session, HttpServletResponse response) throws ServletException, IOException {
+		if(session.isAuthenticated() || !session.getUser().getPermissions().hasAtLeast(UserRole.EDITOR)) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			return;
+		}
+		request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+    }
+
+}
