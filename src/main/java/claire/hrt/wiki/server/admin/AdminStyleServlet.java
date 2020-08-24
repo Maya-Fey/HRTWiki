6a111f6a204e674bf2a58ed393b60941c6862d6d
Claire <foxcoindev@gmail.com>
2020-08-24 21:15:29 +1200

    Added admin.css servlet

---
+++ src/main/java/claire/hrt/wiki/server/admin/AdminStyleServlet.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package claire.hrt.wiki.server.admin;
+
+import claire.hrt.wiki.server.resources.AbstractResourceServlet;
+
+/**
+ * @author Claire
+ *
+ */
+public class AdminStyleServlet extends AbstractResourceServlet {
+
+	private static final long serialVersionUID = 22433823531202542L;
+
+	/**
+	 * 
+	 */
+	public AdminStyleServlet() {
+		super("/webresources/admin.css");
+	}
+
+}
