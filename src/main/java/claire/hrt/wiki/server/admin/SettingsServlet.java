35726a82afef6100018cb9a902063fb22c0ae6cd
Claire <foxcoindev@gmail.com>
2020-08-26 21:07:57 +1200

    Added submit capability to settings.jsp

--- src/main/java/claire/hrt/wiki/server/admin/SettingsServlet.java
+++ src/main/java/claire/hrt/wiki/server/admin/SettingsServlet.java
@@ -9,9 +9,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import claire.hrt.wiki.commons.Null;
 import claire.hrt.wiki.data.Session;
+import claire.hrt.wiki.data.User;
+import claire.hrt.wiki.data.WebState;
 import claire.hrt.wiki.data.enumerate.UserRole;
+import claire.hrt.wiki.data.except.NoSuchKeyException;
 import claire.hrt.wiki.server.AbstractStateServlet;
+import claire.hrt.wiki.server.validate.CharacterValidator;
+import claire.hrt.wiki.server.validate.Field;
+import claire.hrt.wiki.server.validate.LengthValidator;
+import claire.hrt.wiki.server.validate.RequestValidator;
 
 /**
  * @author Claire
@@ -29,6 +37,11 @@ public class SettingsServlet extends AbstractStateServlet {
 	 */
 	public static final String PRONOUN_ALLOWED = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz /";
 	
+	private static final RequestValidator validator = new RequestValidator(
+			new Field("name", true, new LengthValidator(2, 64), new CharacterValidator(NAME_ALLOWED)),
+			new Field("pronouns", true, new LengthValidator(3, 20), new CharacterValidator(PRONOUN_ALLOWED))
+		);
+	
 	@Override
     protected void get(HttpServletRequest request, Session session, HttpServletResponse response) throws ServletException, IOException {
 		if(!session.isAuthenticated() || !session.getUser().getPermissions().hasAtLeast(UserRole.EDITOR)) {
@@ -44,4 +57,25 @@ public class SettingsServlet extends AbstractStateServlet {
 		request.getRequestDispatcher("/WEB-INF/admin/settings.jsp").forward(request, response);
     }
 
+	@Override
+    protected void post(HttpServletRequest request, Session session, HttpServletResponse response) throws ServletException, IOException {
+		if(!session.isAuthenticated() || !session.getUser().getPermissions().hasAtLeast(UserRole.EDITOR)) {
+			response.sendRedirect("/admin/login.jsp");
+			return;
+		}
+		String err = validator.validate(request);
+		if(err.length() > 0) {
+			request.setAttribute("failure_reason", err);
+			this.get(request, session, response);
+			return;
+		}
+		try {
+			User user = session.getUser().changeName(Null.nonNull(request.getParameter("name"))).changePronouns(Null.nonNull(request.getParameter("pronouns")));
+			WebState.INST.users.updateUser(user);
+			session.updateUser(user);
+		} catch (@SuppressWarnings("unused") NoSuchKeyException e) { /**/ }
+		request.setAttribute("success", "true");
+		this.get(request, session, response);
+	}
+	
 }
